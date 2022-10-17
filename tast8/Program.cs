d0fc7254f6c8de14056fdb3aeb0b51c4a5510395
Aleksandr <tabruka@yandex.ru>
2022-10-17 07:45:35 +0300

    Добавил задачу №8

---
+++ tast8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int start = 1;
+Console.WriteLine("Введите число и мы найдем все четные от 1 до Вашего введенного числа");
+int N = int.Parse(Console.ReadLine());
+
+while (start < N)
+{
+    if(start % 2 == 0)
+    {
+        Console.WriteLine($"Число, {start} четное");
+        start ++;
+    }
+    else
+    {
+        start ++;
+    }
+}
